f4bc659c42a618ec4e68f0a56af5d65b91cdfcd7
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-01-21 21:00:51 +0100

    docs: fix minor typos

--- src/volumes/aabb.rs
+++ src/volumes/aabb.rs
@@ -17,7 +17,7 @@ impl<const D: usize> Aabb<D> {
     ///
     /// # Panics
     ///
-    /// Panic if the a component of max is not greater than min
+    /// Panic if `max` is not greater than `min`
     #[must_use]
     pub fn from_min_max(min: impl Into<[f32; D]>, max: impl Into<[f32; D]>) -> Self {
         let min: [f32; D] = min.into();
